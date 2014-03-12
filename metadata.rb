26b44b8c0333b0aa05cd046fda6c15cd6e929e1f
Yoshihide Chubachi <yoshi@chubachi.net>
2014-03-12 22:59:42 +0900

    Run berks cookbook again

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'vagrant_nginx_php_mysql'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures vagrant_nginx_php_mysql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
