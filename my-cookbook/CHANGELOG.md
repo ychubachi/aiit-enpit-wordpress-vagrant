6aa867b1245fe37e07c4f88570964e05fb0943b5
Yoshihide Chubachi <yoshi@chubachi.net>
2014-03-13 14:29:25 +0900

    Berkshelf と Vagrant の連携確立

---
+++ my-cookbook/CHANGELOG.md
@@ -0,0 +1,5 @@
+# my-cookbook cookbook CHANGELOG
+This file is used to list changes made in each version of the my-cookbook cookbook.
+
+## v0.1.0 (2014-03-13)
+- Initial release of my-cookbook
---
+++ my-cookbook/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: my-cookbook
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apt'
+include_recipe 'git'
+include_recipe 'mysql::server'
